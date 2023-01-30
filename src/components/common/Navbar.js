ae02aaba58faa9f71d919d8622af10cb3ea28696
kiritoab <81777954+kiritoab@users.noreply.github.com>
2023-01-30 23:17:14 +0200

    style

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -33,38 +33,6 @@ const Navbar = () => {
       </form>
     </div></div>
   </nav>
-
-    // <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    //   <div className="container-fluid">
-    //     <span>
-    //       <NavLink className="navbar-brand" to="/">
-    //         iShop
-    //       </NavLink>
-    //     </span>
-    //     <span>
-    //       <NavLink className="nav-link active" aria-current="page" to="/">
-    //         Home
-    //       </NavLink>
-    //     </span>
-    //     <span>
-    //       <NavLink className="nav-link" to="/cart">
-    //         Cart
-    //       </NavLink>
-    //       <div className="cart-counter">
-    //         <span>{productsInCart.length}</span>
-    //       </div>
-    //     </span>
-    //    <span>
-    //       <a className="nav-link" href= "#footer"> Contacts </a>
-    //     </span>
-    //     <span>
-    //       <NavLink className="nav-link" to="/login">
-    //         Login
-    //       </NavLink>
-    //     </span>
- 
-    //   </div>
-    // </nav>
   );
 };
 
